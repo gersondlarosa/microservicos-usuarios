fc71eee7a79e50f237f4614aa00fc157ae07bab8
linck <linck@host.docker.internal>
2026-05-06 16:56:37 -0600

    Se realizna cambio ya que no existia la anotación @PutMapping("/{id}") en Alumno controller y en el pom se agrega eureka.instance.prefer-ip-address=true ya que la ip no responida correctamente, se agrega en el pomp la dependencia <dependency> 	<groupId>com.formacionbdi.microservicios.commons</groupId> 	<artifactId>commons-microservicios</artifactId> 	<version>0.0.1-SNAPSHOT</version> 	</dependency> para agregar los commonts

--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,7 +45,7 @@ public class AlumnoController {
 		
 	}
 	
-	@PostMapping("/{id}")
+	@PutMapping("/{id}")
 	public ResponseEntity<?> editar(@RequestBody Alumno alumno, @PathVariable Long id){
 		Optional<Alumno> o = service.findByid(id);
 		
